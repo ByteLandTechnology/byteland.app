4c06908eafbb629b91fdda03a970a7ea98407dc5
byteland-app <byteland-app@users.noreply.github.com>
2025-12-25 12:55:51 +0000

    deploy: a295859f76c8b33f5ebe1062c3b7c3dcf64621b2

--- _next/static/chunks/a4620ff58e211479.js
+++ _next/static/chunks/a4620ff58e211479.js
@@ -1,7 +1,7 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/70ced0187ad6487a.js",
+  "static/chunks/0eb21ae3df9b6345.js",
   "static/chunks/e8cde9e255ff5e90.js",
   "static/chunks/565b84617f849628.js",
   "static/chunks/c89be53acc45d797.css",
-  "static/chunks/turbopack-4304c72adc5294fd.js"
+  "static/chunks/turbopack-f64a22c481c92ecd.js"
 ])
